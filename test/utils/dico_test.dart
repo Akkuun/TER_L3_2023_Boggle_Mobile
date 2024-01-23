7cb669af39bbb2f49094c60a1c8b9ef2ec749b5e
DrHurel <jeremyhurel34@gmail.com>
2024-01-24 00:02:59 +0100

    save

--- test/utils/dico_test.dart
+++ test/utils/dico_test.dart
@@ -5,28 +5,19 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  group("unit test for dictionary", () {
-    test('fun is in the dictionary', () async {
-      Map<LangCode, int> lang = generateLangCode();
-      Decoded decoder = Decoded(lang: lang);
+  group("unit test for dictionary", () async {
+    Map<LangCode, int> lang = generateLangCode();
+    Decoded decoder = Decoded(lang: lang);
 
-      Dictionary dictionary =
-          Dictionary(path: "assets/dictionary/global.json", decoder: decoder);
+    Dictionary dictionary =
+        Dictionary(path: "assets/dictionary/global.json", decoder: decoder);
 
-      await dictionary.load();
-
+    await dictionary.load();
+    test('fun is in the dictionary', () {
       expect(dictionary.contain("FUN"), true);
     });
 
-    test('ZEFOB is not in the dictionary', () async {
-      Map<LangCode, int> lang = generateLangCode();
-      Decoded decoder = Decoded(lang: lang);
-
-      Dictionary dictionary =
-          Dictionary(path: "assets/dictionary/global.json", decoder: decoder);
-
-      await dictionary.load();
-
+    test('ZEFOB is not in the dictionary', () {
       expect(dictionary.contain("ZEFOB"), false);
     });
   });
