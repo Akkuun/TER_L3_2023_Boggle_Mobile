42265f7c494cc5abdf332a2e7986f33a215b9ed7
DrHurel <jeremyhurel34@gmail.com>
2024-01-23 14:39:41 +0100

    add binary package

---
+++ lib/utils/decode.dart
@@ -0,0 +1,21 @@
+import 'package:binary/binary.dart';
+
+class Decoded {
+  final Map<LangCode, int> lang;
+
+  Decoded({required this.lang});
+
+  bool endAWord(int code) {
+    return code & 1 << 8 > 0;
+  }
+
+  bool isFrom(int code, LangCode langCode) {
+    return code & lang[langCode]! > 0;
+  }
+
+  Int8 getRunesFrom(int code) {
+    return Int8(code);
+  }
+}
+
+enum LangCode { FR, RM, EN, SP }
