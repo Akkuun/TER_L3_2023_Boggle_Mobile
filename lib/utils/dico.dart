e9c23389a68432f351daf5e67527d20005371e74
DrHurel <jeremyhurel34@gmail.com>
2024-01-23 18:32:15 +0100

    wip dictionary tool

--- lib/utils/dico.dart
+++ lib/utils/dico.dart
@@ -26,14 +26,21 @@ class Dictionary {
       return false;
     }
     List<dynamic> temp = dictionary!;
-
-    for (int l in word.runes) {
+    int count = word.length;
+    for (final l in word.runes) {
+      count--;
       if (temp.length > 1) {
         if (temp[1] is int) {
         } else {
           for (int i = 1; i < temp.length; i++) {
             if (temp[i] is int) {
-              if (decoder.getRunesFrom(temp[i]) == Int8(l)) {}
+              if (decoder.getRunesFrom(temp[i]) == Int8(l)) {
+                if (count != 0) {
+                  return false;
+                } else {
+                  return decoder.endAWord(temp[i]);
+                }
+              }
             } else {
               if (decoder.getRunesFrom(temp[i][0]) == Int8(l)) {
                 temp = temp[i][0];
@@ -44,6 +51,6 @@ class Dictionary {
       }
     }
 
-    return false;
+    return decoder.endAWord(temp[0]);
   }
 }
