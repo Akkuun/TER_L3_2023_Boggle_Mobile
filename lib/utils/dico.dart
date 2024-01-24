b3357cef3134ed912c76225d2687b16857c6774c
DrHurel <jeremyhurel34@gmail.com>
2024-01-24 06:25:01 +0100

    fix canCreate & remove useless type check

--- lib/utils/dico.dart
+++ lib/utils/dico.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:binary/binary.dart';
 import 'package:bouggr/utils/decode.dart';
@@ -7,19 +8,22 @@ import 'package:flutter/services.dart';
 class Dictionary {
   final String path;
   final Decoded decoder;
-  late dynamic dictionary;
+  late dynamic dictionary; // dictionary[0] = key  dictionary[1] = children
 
   Dictionary({required this.path, required this.decoder});
 
+  /// The function loads a JSON file from the specified path and decodes it into a dictionary.
   Future load() async {
     final jsonString = await rootBundle.loadString(path);
     dictionary = jsonDecode(jsonString);
   }
 
+  /// The function "unload" sets the variable "dictionary" to null.
   void unload() {
     dictionary = null;
   }
 
+  /// The function "contain" check if a string is in the dictionary
   bool contain(String word) {
     if (dictionary == null) {
       return false;
@@ -31,30 +35,25 @@ class Dictionary {
       if (temp.length > 1) {
         dynamic children = temp[1];
         // the current node has children
-        if (children.runtimeType == int) {
-          // is a leaf
-          return count == 0 && decoder.isEndingAWord(temp[1]);
-          //check if last letter of the word & if is completing a word
-        } else {
-          bool update = false;
-          for (int i = 0; i < children.length; i++) {
-            if (children[i].runtimeType == int) {
-              //is a leaf
-              if (decoder.getRunesFrom(children[i]) == Int8(l)) {
-                return count == 0 && decoder.isEndingAWord(temp[1][i]);
-                //check if last letter of the word & if is completing a word
-              }
-            } else {
-              if (decoder.getRunesFrom(children[i][0]) == Int8(l)) {
-                temp = children[i];
-                update = true;
-                break;
-              }
+
+        bool update = false;
+        for (int i = 0; i < children.length; i++) {
+          if (children[i].runtimeType == int) {
+            //is a leaf
+            if (decoder.getRunesFrom(children[i]) == Int8(l)) {
+              return count == 0 && decoder.isEndingAWord(temp[1][i]);
+              //check if last letter of the word & if is completing a word
+            }
+          } else {
+            if (decoder.getRunesFrom(children[i][0]) == Int8(l)) {
+              temp = children[i];
+              update = true;
+              break;
             }
           }
-          if (!update) {
-            return false;
-          }
+        }
+        if (!update) {
+          return false;
         }
       } else {
         return false; // more letter than node for this word
@@ -64,34 +63,40 @@ class Dictionary {
     return decoder.isEndingAWord(temp[0]);
   }
 
+  /// The function "canCreate" check if a string can be completed to create a word in the dictionary
   bool canCreate(String word) {
     if (dictionary == null) {
       return false;
     }
-    List<dynamic> temp = dictionary!;
+
+    dynamic temp = dictionary!;
     int count = word.length;
+
     for (final l in word.runes) {
       count--;
       if (temp.length > 1) {
+        dynamic children = temp[1];
         // the current node has children
-        if (temp[1].runtimeType == int) {
-          // is a leaf
-          return count == 0;
-          //check if last letter of the word
-        } else {
-          for (int i = 1; i < temp.length; i++) {
-            if (temp[i].runtimeType == int) {
-              //is a leaf
-              if (decoder.getRunesFrom(temp[i]) == Int8(l)) {
-                return count == 0;
-                //check if last letter of the word
-              }
-            } else {
-              if (decoder.getRunesFrom(temp[i][0]) == Int8(l)) {
-                temp = temp[i][0];
-              }
+
+        bool update = false;
+        for (int i = 0; i < children.length; i++) {
+          if (children[i].runtimeType == int) {
+            //is a leaf
+            if (decoder.getRunesFrom(children[i]) == Int8(l)) {
+              return count == 0;
+              //check if last letter of the word
+            }
+          } else {
+            if (decoder.getRunesFrom(children[i][0]) == Int8(l)) {
+              temp = children[i];
+              update = true;
+              break; //child found
             }
           }
+        }
+        if (!update) {
+          // check if we found a child that match the letter (l)
+          return false;
         }
       } else {
         return false; // more letter than node for this word
