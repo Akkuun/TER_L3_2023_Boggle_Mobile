a1746159bd1b7085ebf4a2922f4f7f72c869a19d
DrHurel <jeremyhurel34@gmail.com>
2024-02-06 12:05:31 +0100

    Save better placement

--- lib/components/pop_up_game_menu.dart
+++ lib/components/pop_up_game_menu.dart
@@ -18,43 +18,50 @@ class PopUpGameMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PopUp<GameServices>(
-      child: Center(
-        child: Container(
-          height: min(MediaQuery.of(context).size.width * 0.8,
-              MediaQuery.of(context).size.height * 0.8),
-          width: min(MediaQuery.of(context).size.width * 0.8,
-              MediaQuery.of(context).size.height * 0.8),
-          decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 181, 224, 255),
-              borderRadius: BorderRadius.circular(10)),
-          child: Column(
-            children: [
-              BtnBoggle(
-                onPressed: () {
-                  Provider.of<GameServices>(context, listen: false)
-                      .toggle(false);
-                  Provider.of<TimerServices>(context, listen: false).start();
-                },
-                text: "X",
-                btnType: BtnType.square,
-              ),
-              BtnBoggle(
-                onPressed: () {
-                  Provider.of<GameServices>(context, listen: false).stop();
-                  Provider.of<NavigationServices>(context, listen: false)
-                      .goToPage(PageName.home);
-                },
-                text: "new game",
-              ),
-              BtnBoggle(
+      child: Positioned(
+        top: MediaQuery.of(context).size.height * 0.5 -
+            min(MediaQuery.of(context).size.width * 0.8,
+                MediaQuery.of(context).size.height * 0.8),
+        left: min(MediaQuery.of(context).size.width * 0.1,
+            MediaQuery.of(context).size.height * 0.1),
+        child: Center(
+          child: Container(
+            height: min(MediaQuery.of(context).size.width * 0.8,
+                MediaQuery.of(context).size.height * 0.8),
+            width: min(MediaQuery.of(context).size.width * 0.8,
+                MediaQuery.of(context).size.height * 0.8),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 181, 224, 255),
+                borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              children: [
+                BtnBoggle(
+                  onPressed: () {
+                    Provider.of<GameServices>(context, listen: false)
+                        .toggle(false);
+                    Provider.of<TimerServices>(context, listen: false).start();
+                  },
+                  text: "X",
+                  btnType: BtnType.square,
+                ),
+                BtnBoggle(
                   onPressed: () {
                     Provider.of<GameServices>(context, listen: false).stop();
                     Provider.of<NavigationServices>(context, listen: false)
                         .goToPage(PageName.home);
                   },
-                  text: "Home",
-                  btnType: BtnType.secondary),
-            ],
+                  text: "new game",
+                ),
+                BtnBoggle(
+                    onPressed: () {
+                      Provider.of<GameServices>(context, listen: false).stop();
+                      Provider.of<NavigationServices>(context, listen: false)
+                          .goToPage(PageName.home);
+                    },
+                    text: "Home",
+                    btnType: BtnType.secondary),
+              ],
+            ),
           ),
         ),
       ),
