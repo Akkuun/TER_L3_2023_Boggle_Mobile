7f24c203008968fa819935a02acf798f9ad4863e
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2024-03-04 15:09:40 +0100

    Add dependencies, create a multiplayer create or join page

--- app/lib/pages/multiplayer.dart
+++ app/lib/pages/multiplayer.dart
@@ -1,103 +1,142 @@
 //components
 
-import 'package:bouggr/components/grille.dart';
-
-import 'package:bouggr/components/scoreboard.dart';
-import 'package:bouggr/components/words_found.dart';
-import 'package:bouggr/components/title.dart';
-
 //globals
-import 'package:bouggr/global.dart';
 
 //services
+
+import 'package:bouggr/components/btn.dart';
+import 'package:bouggr/components/title.dart';
+import 'package:bouggr/pages/page_name.dart';
 import 'package:bouggr/providers/game.dart';
 
 //utils
 import 'package:bouggr/providers/timer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-import 'package:bouggr/utils/word_score.dart';
-import 'package:bouggr/utils/dico.dart';
+
 //flutter
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../components/action_and_timer.dart';
-import '../components/pop_up_game_menu.dart';
 import 'package:bouggr/components/bottom_buttons.dart';
+
+import '../providers/navigation.dart';
 
 class GameMultiplayerPage extends StatefulWidget {
   const GameMultiplayerPage({super.key});
 
   @override
-  State<GameMultiplayerPage> createState() => _GamePageState();
+  State<GameMultiplayerPage> createState() => _GameMultiplayerPageState();
 }
 
-class _GamePageState extends State<GameMultiplayerPage> {
-  late BoggleGrille boggleGrille;
-  final List<String> _previousWords = [];
-  int _score = 0;
-  int _strikes = 0;
-
-  late Dictionary _dictionary;
-
-  @override
-  void initState() {
-    super.initState();
-    var lang = Provider.of<GameServices>(context, listen: false).language;
-
-    _dictionary = Globals.selectDictionary(lang);
-    _dictionary.load();
-    List<String> selectedLetter = Globals.selectDiceSet(lang).roll();
-
-    boggleGrille = BoggleGrille(
-        letters: selectedLetter,
-        onWordSelectionEnd: _endWordSelection,
-        isWordValid: _isWordValid);
-  }
-
-  bool _isWordValid(String word) {
-    if (word.length < 3) {
-      return false;
-    }
-    return _dictionary.contain(word);
-  }
-
-  bool _endWordSelection(String word) {
-    if (_previousWords.contains(word) || !_isWordValid(word)) {
-      setState(() {
-        _strikes++;
-      });
-      return false;
-    }
-    _updateScore(wordScore(word));
-    _previousWords.add(word);
-    return true;
-  }
-
-  int _updateScore(int wordScore) {
-    setState(() {
-      _score += wordScore;
-    });
-    return _score;
-  }
-
-  List<int> _countWordsByLength() {
-    if (_previousWords.isEmpty) {
-      return [];
-    }
-    int max =
-        _previousWords.map((e) => e.length).reduce((a, b) => a > b ? a : b);
-    List<int> count = List.filled(max - 2, 0);
-    for (var word in _previousWords) {
-      count[word.length - 3]++;
-    }
-    return count;
-  }
+class _GameMultiplayerPageState extends State<GameMultiplayerPage> {
+  String? _gameUID = '';
+  BtnType _btnType = BtnType.secondary;
 
   @override
   Widget build(BuildContext context) {
-    var timerServices = context.watch<TimerServices>();
-    var gameServices = context.watch<GameServices>();
-    return const BottomButtons(child: Text("Multiplayer"));
+    final router = Provider.of<NavigationServices>(context, listen: false);
+    User? user;
+    try {
+      user = FirebaseAuth.instance.currentUser;
+      if (user == null) {
+        router.goToPage(PageName.login);
+      }
+    } catch (e) {
+      router.goToPage(PageName.login);
+    }
+    FirebaseDatabase database = FirebaseDatabase.instance;
+
+    final playerUID = FirebaseAuth.instance.currentUser!.uid;
+
+    var playerRef = database.ref('players/$playerUID');
+    playerRef.set({
+      'email': user!.email,
+      'score': 0,
+    });
+
+    return BottomButtons(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const AppTitle(),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Mul",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontFamily: 'Jua',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  "ti",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 30,
+                    fontFamily: 'Jua',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  "player",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontFamily: 'Jua',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+            BtnBoggle(
+              onPressed: () {
+                router.goToPage(PageName.game);
+              },
+              btnSize: BtnSize.large,
+              text: "Create a game",
+            ),
+            const Text(
+              "or",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontFamily: 'Jua',
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: TextField(
+                onChanged: (value) {
+                  setState(() {
+                    _gameUID = value;
+                    _btnType = _gameUID!.isNotEmpty ? BtnType.primary : BtnType.secondary;
+                  });
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Game code',
+                ),
+              ),
+            ),
+            BtnBoggle(
+              onPressed: () {
+                router.goToPage(PageName.home);
+              },
+              btnSize: BtnSize.large,
+              text: "Join a game",
+              btnType: _btnType,
+            ),
+          ],
+        )
+    );
   }
 }
