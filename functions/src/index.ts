7daa2c2af03cc07276f20ffc37c3a8bffc91e35f
DrHurel <jeremyhurel34@gmail.com>
2024-03-08 14:04:15 +0100

    Add cloud_functions dependency

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,7 @@
-import { Game } from "fivem-js";
+
 import { GameHandler } from "./services/game_handler";
 
-    const {onValueCreated} = require("firebase-functions/v2/database");
-const {logger} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require("firebase-admin");
 
@@ -11,41 +9,58 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 // Init firestore
 
+interface User {
+    email: string,
+    score: number,
+    leader: boolean,
+}
+
+interface GameInterface {
+    status: string;
+    players: Map<string, User>;
+    letters: string;
+    lang: number;
+}
+
+
 
 //cron task to remove games after 10 minutes
 
-export const CreateGame = onRequest(async (req : any, res:any) => {
+
+
+export const CreateGame = onRequest(async (req: any, res: any) => {
     const data = req.body;
     const game = admin.database().ref("/games").push();
     const gameId = game.key;
-    game.set({
-        status: "waiting",
-        players: [data.userId],
-        creator: data.userId,
-        gameId,
-        createdAt: Date.now()
-    });
+    const payload: GameInterface = {
+        status: "created",
+        players: new Map<string, User>(),
+        letters: data.letters,
+        lang: data.lang,
+    };
+    payload.players.set(data.userId, { email: data.email, score: 0, leader: true });
+    game.set(payload);
 
     const db = admin.database().ref(`/games/${gameId}`);
 
-    GameHandler.createGame(db,gameId, data.grid, data.lang);
+    GameHandler.createGame(db, gameId, data.letters, data.lang);
 
     res.status(200).send(gameId);
 }
 );
 
 
-export const StartGame = onRequest(async (req : any, res:any) => {
+export const StartGame = onRequest(async (req: any, res: any) => {
     const data = req.body;
 
-   
 
-    const game =  GameHandler.getGame(data.gameId);
+
+    const game = GameHandler.getGame(data.gameId);
     const gameData = await game.Data;
     if (gameData.status === "started") {
         res.status(400).send("Game already started");
     } else {
-        if (gameData.players.length <2) {
+        if (gameData.players.length < 2) {
             res.status(400).send("Not enough players");
         } else {
             // start game
@@ -58,7 +73,7 @@ export const StartGame = onRequest(async (req : any, res:any) => {
 );
 
 
-export const JoinGame = onRequest(async (req : any, res:any) => {
+export const JoinGame = onRequest(async (req: any, res: any) => {
     const data = req.body;
     const game = admin.database().ref(`/games/${data.gameId}`);
     const gameData = await game.get();
@@ -69,14 +84,14 @@ export const JoinGame = onRequest(async (req : any, res:any) => {
             res.status(400).send("Game full");
         } else {
             // join game
-            game.update({players: [...gameData.players, data.userId]});
+            game.update({ players: [...gameData.players, data.userId] });
 
             res.status(200).send("Game joined");
         }
     }
 });
 
-export const LeaveGame = onRequest(async (req : any, res:any) => {
+export const LeaveGame = onRequest(async (req: any, res: any) => {
     const data = req.body;
     const game = admin.database().ref(`/games/${data.gameId}`);
     const gameData = await game.get();
@@ -84,14 +99,14 @@ export const LeaveGame = onRequest(async (req : any, res:any) => {
         res.status(400).send("Game already started");
     } else {
         // leave game
-        game.update({players: gameData.players.filter((player: string) => player !== data.userId)});
+        game.update({ players: gameData.players.filter((player: string) => player !== data.userId) });
 
         res.status(200).send("Game left");
     }
 });
 
 
-export const sendWord = onRequest(async (req : any, res:any) => {
+export const sendWord = onRequest(async (req: any, res: any) => {
     const data = req.body;
     const game = admin.database().ref(`/games/${data.gameId}`);
     const gameData = await game.get();
@@ -99,7 +114,7 @@ export const sendWord = onRequest(async (req : any, res:any) => {
         res.status(400).send("Game already started");
     } else {
         // leave game
-        game.update({word: data.word});
+        game.update({ word: data.word });
 
         res.status(200).send("Word sent");
     }
