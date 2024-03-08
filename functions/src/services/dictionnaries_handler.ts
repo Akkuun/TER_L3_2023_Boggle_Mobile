7af29927bc118a77824701815bfa4415c877fb31
DrHurel <jeremyhurel34@gmail.com>
2024-03-08 17:05:37 +0100

    Add dictionaries handler and update game handler

--- functions/src/services/dictionnaries_handler.ts
+++ functions/src/services/dictionnaries_handler.ts
@@ -1,12 +1,16 @@
 import { Dictionary } from "../entity/dictionnary";
 
-export class DictionariesHandler{
+export class DictionariesHandler {
 
-    private static dictionaries: Map<number, Dictionary> = new Map<number, Dictionary>();
+    constructor() { }
 
+    private dictionaries: Map<number, Dictionary> = new Map<number, Dictionary>();
 
+    public addDictionary(lang: number, dictionary: Dictionary) {
+        this.dictionaries.set(lang, dictionary);
+    }
 
-    public static getDictionary(lang: number) {
+    public getDictionary(lang: number) {
         return this.dictionaries.get(lang) as Dictionary;
     }
 }
