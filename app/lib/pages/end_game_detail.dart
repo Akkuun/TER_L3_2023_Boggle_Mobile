92f3dae15d56234e6dd42ef633445655cc84cfe2
DrHurel <jeremyhurel34@gmail.com>
2024-03-12 17:07:00 +0100

    Add JoinMulti widget and update page routing

---
+++ app/lib/pages/end_game_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EndGameDetail extends StatelessWidget {
+  const EndGameDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
