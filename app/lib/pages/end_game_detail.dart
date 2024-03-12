82018304ce2a7e89c7e774cf2c420f4c0d0517ba
Vincent Bernardon <flyber11@gmail.com>
2024-03-12 18:04:07 +0100

    Mise à jour de la mise en page de EndGameDetail

--- app/lib/pages/end_game_detail.dart
+++ app/lib/pages/end_game_detail.dart
@@ -28,77 +28,105 @@ class EndGameDetail extends StatelessWidget {
 
     WordsFound wordsFound = const WordsFound();
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: ShapeDecoration(
-              color: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+    return Container(
+      color: const Color.fromARGB(255, 169, 224, 255),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                shadows: const [
+                  BoxShadow(
+                    color: Color(0x3F000000),
+                    blurRadius: 4,
+                    offset: Offset(0, 4),
+                    spreadRadius: 0,
+                  )
+                ],
               ),
-              shadows: const [
-                BoxShadow(
-                  color: Color(0x3F000000),
-                  blurRadius: 4,
-                  offset: Offset(0, 4),
-                  spreadRadius: 0,
-                )
-              ],
-            ),
-            width: MediaQuery.of(context).size.width * 0.9,
-            height: MediaQuery.of(context).size.height * 0.3,
-            child: wordsFound,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: ShapeDecoration(
-              color: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("Score : ${gameServices.score}"),
+                      const Text("Score Max : NaN"),
+                    ],
+                  ),
+                  Text("Plus long mot trouvé : ${gameServices.longestWord}"),
+                  const Text("Plus long mot trouvable : pas dispo"),
+                  BtnBoggle(
+                    onPressed: () {
+                      gameServices.stop();
+      
+                      GameDataStorage.saveGameResult(gameResult);
+      
+                      timerServices.resetProgress();
+                      gameServices.reset();
+                      navigationServices.goToPage(PageName.home); //TODO a rediriger vers la popup de tout les mots qui s'affiche sur la grille
+                    },
+                    text: "new game",
+                  ),
+                ],
               ),
-              shadows: const [
-                BoxShadow(
-                  color: Color(0x3F000000),
-                  blurRadius: 4,
-                  offset: Offset(0, 4),
-                  spreadRadius: 0,
-                )
-              ],
             ),
-            width: MediaQuery.of(context).size.width * 0.9,
-            height: MediaQuery.of(context).size.height * 0.45,
-            child: wordsFound,
           ),
-        ),
-        BtnBoggle(
-          onPressed: () {
-            gameServices.stop();
-
-            GameDataStorage.saveGameResult(gameResult);
-
-            timerServices.resetProgress();
-            gameServices.reset();
-            navigationServices.goToPage(PageName.home);
-          },
-          text: "new game",
-        ),
-        BtnBoggle(
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                shadows: const [
+                  BoxShadow(
+                    color: Color(0x3F000000),
+                    blurRadius: 4,
+                    offset: Offset(0, 4),
+                    spreadRadius: 0,
+                  )
+                ],
+              ),
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.45,
+              child: wordsFound,
+            ),
+          ),
+          BtnBoggle(
             onPressed: () {
               gameServices.stop();
-
+      
               GameDataStorage.saveGameResult(gameResult);
-              gameServices.reset();
+      
               timerServices.resetProgress();
+              gameServices.reset();
               navigationServices.goToPage(PageName.home);
             },
-            text: "Home",
-            btnType: BtnType.secondary),
-      ],
+            text: "new game",
+          ),
+          BtnBoggle(
+              onPressed: () {
+                gameServices.stop();
+      
+                GameDataStorage.saveGameResult(gameResult);
+                gameServices.reset();
+                timerServices.resetProgress();
+                navigationServices.goToPage(PageName.home);
+              },
+              text: "Home",
+              btnType: BtnType.secondary),
+        ],
+      ),
     );
   }
 }
