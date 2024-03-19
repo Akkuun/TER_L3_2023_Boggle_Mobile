dbfdce1bda683127aafe6b75a324798846e6d4f1
DrHurel <jeremyhurel34@gmail.com>
2024-03-19 23:58:17 +0100

    Update libsum.h for x86 and armeabi-v7a architectures

--- android/src/main/jniLibs/x86/libsum.h
+++ android/src/main/jniLibs/x86/libsum.h
@@ -2,7 +2,6 @@
 
 /* package go_code */
 
-
 #line 1 "cgo-builtin-export-prolog"
 
 #include <stddef.h>
@@ -11,13 +10,16 @@
 #define GO_CGO_EXPORT_PROLOGUE_H
 
 #ifndef GO_CGO_GOSTRING_TYPEDEF
-typedef struct { const char *p; ptrdiff_t n; } _GoString_;
+typedef struct
+{
+  const char *p;
+  ptrdiff_t n;
+} _GoString_;
 #endif
 
 #endif
 
 /* Start of preamble from import "C" comments.  */
-
 
 #line 3 "lib.go"
 #include <string.h>
@@ -26,9 +28,7 @@ typedef struct { const char *p; ptrdiff_t n; } _GoString_;
 
 #line 1 "cgo-generated-wrapper"
 
-
 /* End of preamble from import "C" comments.  */
-
 
 /* Start of boilerplate cgo prologue.  */
 #line 1 "cgo-gcc-export-header-prolog"
@@ -62,28 +62,38 @@ typedef double _Complex GoComplex128;
   static assertion to make sure the file is being used on architecture
   at least with matching size of GoInt.
 */
-typedef char _check_for_32_bit_pointer_matching_GoInt[sizeof(void*)==32/8 ? 1:-1];
+typedef char _check_for_32_bit_pointer_matching_GoInt[1];
 
 #ifndef GO_CGO_GOSTRING_TYPEDEF
 typedef _GoString_ GoString;
 #endif
 typedef void *GoMap;
 typedef void *GoChan;
-typedef struct { void *t; void *v; } GoInterface;
-typedef struct { void *data; GoInt len; GoInt cap; } GoSlice;
+typedef struct
+{
+  void *t;
+  void *v;
+} GoInterface;
+typedef struct
+{
+  void *data;
+  GoInt len;
+  GoInt cap;
+} GoSlice;
 
 #endif
 
 /* End of boilerplate cgo prologue.  */
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-extern int sum(int a, int b);
-extern char** GetAllWord(char* cgrid, void* cdico);
-extern void FreeCStringArray(char** cstr, int size);
-extern void enforce_binding();
+  extern int sum(int a, int b);
+  extern char **GetAllWord(char *cgrid, void *cdico);
+  extern void FreeCStringArray(char **cstr, int size);
+  extern void enforce_binding();
 
 #ifdef __cplusplus
 }
