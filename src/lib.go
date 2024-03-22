740d5b100d55f38407ad0ee484dd462ae150bf05
DrHurel <jeremyhurel34@gmail.com>
2024-03-22 20:47:19 +0100

    Update lib.go and jniLibs files

--- src/lib.go
+++ src/lib.go
@@ -101,7 +101,8 @@ func GetAllWord(cgrid *C.char, cdico *C.void, n *C.int) **C.char {
 	//n is the number of word found, if n < 0, an error occured
 	grid := C.GoString(cgrid)
 	//convert the unsafe.Pointer to []interface{}
-	idico := interface{}(unsafe.Pointer(cdico))
+
+	idico := *(*interface{})(unsafe.Pointer(cdico))
 
 	dico := idico.([]interface{})
 	if len(dico) == 0 {
