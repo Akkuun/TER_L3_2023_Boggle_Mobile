761d572acf23b096fc7d5bdc1491ea223cea99d5
DrHurel <jeremyhurel34@gmail.com>
2024-03-26 10:35:10 +0100

    Add EndGameService and PopUpWordList components

--- app/lib/pages/end_game_detail.dart
+++ app/lib/pages/end_game_detail.dart
@@ -1,6 +1,8 @@
 import 'package:bouggr/components/btn.dart';
+import 'package:bouggr/components/game_page/pop_up_word_list.dart';
 import 'package:bouggr/components/game_page/words_found.dart';
 import 'package:bouggr/pages/page_name.dart';
+import 'package:bouggr/providers/end_game_service.dart';
 import 'package:bouggr/providers/game.dart';
 import 'package:bouggr/providers/navigation.dart';
 import 'package:bouggr/providers/timer.dart';
@@ -28,110 +30,110 @@ class EndGameDetail extends StatelessWidget {
         words: []);
 
     var size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: size.width,
-      color: const Color.fromARGB(255, 169, 224, 255),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: ShapeDecoration(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                shadows: const [
-                  BoxShadow(
-                    color: Color(0x3F000000),
-                    blurRadius: 4,
-                    offset: Offset(0, 4),
-                    spreadRadius: 0,
-                  )
-                ],
-              ),
-              width: size.width * 0.9,
-              height: size.height * 0.3,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text("Grid :  OVERFCEANAEBRUAS"),
-                      Text("Score : ${gameServices.score}"),
-                      const Text("Score Max : NaN"),
+    return Stack(
+      children: [
+        Container(
+          height: size.height,
+          width: size.width,
+          color: const Color.fromARGB(255, 169, 224, 255),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    shadows: const [
+                      BoxShadow(
+                        color: Color(0x3F000000),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      )
                     ],
                   ),
-                  Text("Plus long mot trouvé : ${gameServices.longestWord}"),
-                  const Text("Plus long mot trouvable : pas dispo"),
-                  BtnBoggle(
-                    onPressed: () {
-                      gameServices.stop();
-
-                      GameDataStorage.saveGameResult(gameResult);
-
-                      timerServices.resetProgress();
-                      gameServices.reset();
-                      navigationServices.goToPage(PageName
-                          .home); //TODO a rediriger vers la popup de tout les mots qui s'affiche sur la grille
-                    },
-                    text: "Liste des plus long mots",
-                    btnType: BtnType.third,
+                  width: size.width * 0.9,
+                  height: size.height * 0.3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text("Grid :  OVERFCEANAEBRUAS"),
+                          Text("Score : ${gameServices.score}"),
+                          const Text("Score Max : NaN"),
+                        ],
+                      ),
+                      Text(
+                          "Plus long mot trouvé : ${gameServices.longestWord}"),
+                      const Text("Plus long mot trouvable : pas dispo"),
+                      BtnBoggle(
+                        onPressed: () {
+                          Provider.of<EndGameService>(context, listen: false)
+                              .toggle(true);
+                        },
+                        text: "Liste des plus long mots",
+                        btnType: BtnType.third,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: ShapeDecoration(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
                 ),
-                shadows: const [
-                  BoxShadow(
-                    color: Color(0x3F000000),
-                    blurRadius: 4,
-                    offset: Offset(0, 4),
-                    spreadRadius: 0,
-                  )
-                ],
               ),
-              width: size.width * 0.9,
-              height: size.height * 0.45,
-              child: const AllWordsFound(),
-            ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    shadows: const [
+                      BoxShadow(
+                        color: Color(0x3F000000),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      )
+                    ],
+                  ),
+                  width: size.width * 0.9,
+                  height: size.height * 0.45,
+                  child: const WordsFound(),
+                ),
+              ),
+              BtnBoggle(
+                onPressed: () {
+                  gameServices.stop();
+
+                  GameDataStorage.saveGameResult(gameResult);
+
+                  timerServices.resetProgress();
+                  gameServices.reset();
+                  navigationServices.goToPage(PageName.home);
+                },
+                text: "new game",
+              ),
+              BtnBoggle(
+                  onPressed: () {
+                    gameServices.stop();
+
+                    GameDataStorage.saveGameResult(gameResult);
+                    gameServices.reset();
+                    timerServices.resetProgress();
+                    navigationServices.goToPage(PageName.home);
+                  },
+                  text: "Home",
+                  btnType: BtnType.secondary),
+            ],
           ),
-          BtnBoggle(
-            onPressed: () {
-              gameServices.stop();
-
-              GameDataStorage.saveGameResult(gameResult);
-
-              timerServices.resetProgress();
-              gameServices.reset();
-              navigationServices.goToPage(PageName.home);
-            },
-            text: "new game",
-          ),
-          BtnBoggle(
-              onPressed: () {
-                gameServices.stop();
-
-                GameDataStorage.saveGameResult(gameResult);
-                gameServices.reset();
-                timerServices.resetProgress();
-                navigationServices.goToPage(PageName.home);
-              },
-              text: "Home",
-              btnType: BtnType.secondary),
-        ],
-      ),
+        ),
+        const PopUpWordList(),
+      ],
     );
   }
 }
---
+++ app/lib/providers/end_game_service.dart
@@ -0,0 +1,7 @@
+import 'package:bouggr/components/popup.dart';
+
+import 'package:flutter/material.dart';
+
+class EndGameService extends ChangeNotifier with TriggerPopUp {
+  EndGameService();
+}
