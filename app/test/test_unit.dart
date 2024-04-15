3a3c950a65ae02533d2ff16757b8efaff4e8d65c
Vincent Bernardon <flyber11@gmail.com>
2024-04-15 16:17:25 +0200

    test unitaire pour les grille généré aléatoirement

--- app/test/test_unit.dart
+++ app/test/test_unit.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:bouggr/components/card.dart';
 import 'package:bouggr/pages/page_name.dart';
 import 'package:bouggr/providers/game.dart';
+import 'package:bouggr/utils/dice_set.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -51,7 +54,39 @@ class MockNavigationServices extends Mock implements NavigationServices {
   }
 }
 
+int countLetterInDiceSet(DiceSet diceSet, String letter) {
+  int count = 0;
+  for (List<String> dice in diceSet.dices) {
+    for (String l in dice) {
+      if (l == letter) {
+        count++;
+        break;
+      }
+    }
+  }
+  return count;
+}
+
 void main() {
+  DiceSet listeDe = DiceSet(dices: [
+    ["E", "T", "U", "K", "N", "O"],
+    ["E", "V", "G", "T", "I", "N"],
+    ["D", "E", "C", "A", "M", "P"],
+    ["I", "E", "L", "R", "U", "W"],
+    ["E", "H", "I", "F", "S", "E"],
+    ["R", "E", "C", "A", "L", "S"],
+    ["E", "N", "T", "D", "O", "S"],
+    ["O", "F", "X", "R", "I", "A"],
+    ["N", "A", "V", "E", "D", "Z"],
+    ["E", "I", "O", "A", "T", "A"],
+    ["G", "L", "E", "N", "Y", "U"],
+    ["B", "M", "A", "Q", "J", "O"],
+    ["T", "L", "I", "B", "R", "A"],
+    ["S", "P", "U", "L", "T", "E"],
+    ["A", "I", "M", "S", "O", "R"],
+    ["E", "N", "H", "R", "I", "S"]
+  ]);
+
   /*group('HomePage unitaire', () {
     testWidgets('Test pour vérifier si on est sur la page HomePage',
         (WidgetTester tester) async {
@@ -85,8 +120,6 @@ void main() {
   });*/
 
   group('BoggleGrille', () {
-    //fiare test sur une grille qui se génère
-    //faire test sur tout les mots trouver sur grille
 /*
     test('isWordValid retourn faux si le mots fait moins de 3 lettre', () {
       BoggleGrilleState boggleGrilleState = BoggleGrilleState();
@@ -109,7 +142,7 @@ void main() {
         'isWordValid retourne faux si le mot fait au moins 3 lettres et n\'est pas dans le dictionnaire',
         () {});*/
 
-    TestWidgetsFlutterBinding.ensureInitialized();
+    /*TestWidgetsFlutterBinding.ensureInitialized();
 
     test('trouver tout les mots d\'une grille', () async {
       List<String> grid = [
@@ -131,20 +164,70 @@ void main() {
         'E'
       ];
       Dictionary dictionary = Dictionary(
-          path: 'assets/dictionary/fr_dico.json',
+          path: './fr_dico.json',
           decoder: Decoded(lang: generateLangCode()));
-      dictionary.load();
-      List<Word> words = await getAllWords2(grid, dictionary);
+      await dictionary.load();
+      while (dictionary.dictionary == null) {
+        print('loading dictionary');
+        sleep(Durations.extralong1);
+        print('dictionary loaded');
+      }
+      List<Word>? words;
 
-      print(words.length);
+      await getAllWords2(grid, dictionary).then((value) => {words = value});
+      while (words == null) {
+        print('loading words');
+        sleep(Durations.extralong1);
+        print('words loaded');
+      }
 
-      expect(words.length, 10);
+      print(words!.length);
+
+      expect(words!.length, 10);
+    });*/
+
+    test('grille contient un bon nombre de E', () {
+      List<String> letters = listeDe.roll();
+      int count = 0;
+      for (String letter in letters) {
+        if (letter == 'E') {
+          count++;
+        }
+      }
+
+      print('Nombre de E: $count');
+      //au maximum 12 fois la lettre E
+      expect(count, lessThanOrEqualTo(countLetterInDiceSet(listeDe, 'E')));
+    });
+
+    test(
+        'vérification que TOUTE les lettres sont en bon nombre dans une grille aléatoire',
+        () {
+      List<String> letters = listeDe.roll();
+      print(letters);
+      Map<String, int> count = <String,
+          int>{}; //Map<lettre, nombre de fois que la lettre est présente>
+      for (String letter in letters) {
+        if (count.containsKey(letter)) {
+          count[letter] = count[letter]! + 1;
+        } else {
+          count[letter] = 1;
+        }
+      }
+
+      print(count);
+      //chaque lettre doit être présente 1 fois
+      for (String letter in count.keys) {
+        print('Nombre de $letter: ${count[letter]}');
+        expect(count[letter],
+            lessThanOrEqualTo(countLetterInDiceSet(listeDe, letter)));
+      }
     });
   });
 
   //Fermer l'application car les tests se sont effectuer
-  tearDownAll(() {
+  /*tearDownAll(() {
     print('Fermeture de l\'application');
     SystemNavigator.pop();
-  });
+  });*/
 }
