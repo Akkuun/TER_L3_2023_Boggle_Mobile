e7bc7159ccf70ba2f6973e28bc7ab5e6b4aad9ec
DrHurel <jeremyhurel34@gmail.com>
2024-04-16 19:27:17 +0200

    save

--- functions/src/routes/check_word.ts
+++ functions/src/routes/check_word.ts
@@ -6,11 +6,10 @@ import * as admin from "firebase-admin";
 
 import { SendWordI, recreateWord } from "../utils/lang";
 
-import { log } from "firebase-functions/logger";
-
 
 export const check_word = (dictionariesHandler: DictionariesHandler) => async (req: any) => {
-    log("sendWord called with", req.data)
+
+
     const data = req.data as SendWordI;
     const game = admin.database().ref(`/games/${data.gameId}`);
     const gameData = await game.get();
@@ -18,13 +17,28 @@ export const check_word = (dictionariesHandler: DictionariesHandler) => async (r
         return 3;
     }
 
-    log("gameData game found")
+    //game is started && player is in game && game is not finished
+    if ((await game.child("status").get()).val() != "started") {
+        return 2;
+    }
+
+    const player = game.child("players/" + data.userId);
+    const playerData = await player.get();
+    if (!playerData.exists()) {
+        return 4;
+    }
+
+    const endTime = (await game.child("end_time").get()).val();
+    if (endTime < Date.now()) {
+        return 5;
+    }
+
+
+
 
     const grid = (await game.child("letters").get()).val();
     const word = data.word;
     const wordStr = recreateWord(grid, word);
-
-    log("wordStr", wordStr)
 
 
     const dico = dictionariesHandler.getDictionary((await game.child("lang").get()).val());
@@ -33,6 +47,9 @@ export const check_word = (dictionariesHandler: DictionariesHandler) => async (r
     try {
         if (dico.contain(wordStr)) {
             await game.child("players/" + data.userId).push({ word: wordStr });
+            //update score
+            const score = (await player.child("score").get()).val();
+            player.update({ score: score + wordStr.length });
 
             return 0;
         }
@@ -42,7 +59,7 @@ export const check_word = (dictionariesHandler: DictionariesHandler) => async (r
             return 1;
         }
     } catch (e) {
-        log("error", e)
+
         return 1;
     }
 }
