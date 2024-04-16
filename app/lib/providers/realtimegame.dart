d240374487b6e74d7f85fab63f71955afb009cee
DrHurel <jeremyhurel34@gmail.com>
2024-04-16 23:06:28 +0200

    add provider

--- app/lib/providers/realtimegame.dart
+++ app/lib/providers/realtimegame.dart
@@ -1,3 +1,39 @@
-class RealtimeGameProvider extends ChangeNotifier {
+import 'dart:collection';
 
+import 'package:flutter/material.dart';
+
+class RealtimeGameProvider extends ChangeNotifier {
+  HashMap<String, dynamic> _players = HashMap<String, dynamic>();
+
+  void initListeners() {
+    // Ici on va initialiser les listeners pour écouter les changements
+    // dans la base de données
+  }
+
+  void _updatePlayers(HashMap<String, dynamic> players) {
+    _players = players;
+    notifyListeners();
+  }
+
+  bool _isUpdated(HashMap<String, dynamic> players) {
+    for (var player in players.keys) {
+      if (_players[player] == null) {
+        return true;
+      }
+    }
+
+    if (_players.length != players.length) {
+      return true;
+    }
+
+    return false;
+  }
+
+  void _listenerEvent(dynamic event) {
+    if (event is HashMap<String, dynamic>) {
+      if (_isUpdated(event)) {
+        _updatePlayers(event);
+      }
+    }
+  }
 }
